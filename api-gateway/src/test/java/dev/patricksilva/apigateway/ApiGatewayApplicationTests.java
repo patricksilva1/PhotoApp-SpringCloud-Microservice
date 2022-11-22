dfe271a3be65e0ebbdf5d93b5bcbf122a9a75730
patricksilva1 <patricksilva5160@gmail.com>
2022-11-22 16:54:23 -0300

    adding api-gateway

---
+++ api-gateway/src/test/java/dev/patricksilva/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patricksilva.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
