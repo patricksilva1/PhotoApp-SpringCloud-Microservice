e71f30ab1d18a727fc699c7c995d079565b13c0f
patricksilva1 <patricksilva5160@gmail.com>
2022-11-23 18:50:26 -0300

    Task: lb; Load balancing working now

--- photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/ui/controller/UsersController.java
+++ photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/ui/controller/UsersController.java
@@ -1,5 +1,7 @@
 package dev.patricksilva.photoappapiusers.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status/check")
-    public String status(){
-        return "Working!";
+    public String status() {
+        return "Working on port " + env.getProperty("local.server.port");
     }
 }
