1643a1c99edc46f3fa4958091dfb9fc2a57ec5ff
patricksilva1 <patricksilva5160@gmail.com>
2022-11-27 19:12:24 -0300

    PhotoApp: task #30 - Add h2-Database

--- photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/ui/controller/UsersController.java
+++ photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/ui/controller/UsersController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,13 +34,13 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+    public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = modelMapper.map(userDetails, UserDto.class);
         usersService.createUser(userDto);
 
-        return "Create user is called.";
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
