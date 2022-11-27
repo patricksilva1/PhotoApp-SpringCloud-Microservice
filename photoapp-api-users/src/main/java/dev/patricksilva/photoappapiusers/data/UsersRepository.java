4969220099c0c9d7f31dda2cfe78f41e40747635
patricksilva1 <patricksilva5160@gmail.com>
2022-11-27 12:31:07 -0300

    Repository

---
+++ photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/data/UsersRepository.java
@@ -0,0 +1,7 @@
+package dev.patricksilva.photoappapiusers.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+
+}
