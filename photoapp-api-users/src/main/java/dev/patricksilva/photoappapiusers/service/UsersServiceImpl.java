10ef090142a54a63a8e79d952fb785fa75488a18
patricksilva1 <patricksilva5160@gmail.com>
2022-11-26 22:03:33 -0300

    UUID and jpa

--- photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/service/UsersServiceImpl.java
+++ photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/service/UsersServiceImpl.java
@@ -1,11 +1,15 @@
 package dev.patricksilva.photoappapiusers.service;
 
+import java.util.UUID;
+
 import dev.patricksilva.photoappapiusers.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
 
     @Override
     public UserDto createUser(UserDto userDetails) {
+
+        userDetails.setUserId(UUID.randomUUID().toString());
         return null;
     }
 
