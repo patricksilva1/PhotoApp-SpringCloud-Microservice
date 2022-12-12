bc3d0380491889b0c52e7b5e73cc502198fe9926
patricksilva1 <patricksilva5160@gmail.com>
2022-12-11 21:19:27 -0300

    Adding support for xml and json

--- photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/ui/controller/UsersController.java
+++ photoapp-api-users/src/main/java/dev/patricksilva/photoappapiusers/ui/controller/UsersController.java
@@ -7,12 +7,14 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import dev.patricksilva.photoappapiusers.service.UsersService;
 import dev.patricksilva.photoappapiusers.shared.UserDto;
@@ -35,6 +37,10 @@ public class UsersController {
     }
 
     @PostMapping
+    (
+    consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},    
+    produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
     public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
