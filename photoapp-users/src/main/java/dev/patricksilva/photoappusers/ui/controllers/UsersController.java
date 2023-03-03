c9440a0a7eb0071e0be91d40bfcfe42c89592021
patricksilva1 <patricksilva5160@gmail.com>
2023-03-03 18:23:58 -0300

    Implementing RabbitMQ

--- photoapp-users/src/main/java/dev/patricksilva/photoappusers/ui/controllers/UsersController.java
+++ photoapp-users/src/main/java/dev/patricksilva/photoappusers/ui/controllers/UsersController.java
@@ -31,7 +31,8 @@ public class UsersController {
 
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working on port " + env.getProperty("local.server.port");
+		return "Working on port " + env.getProperty("local.server.port") + ", with token = "
+				+ env.getProperty("token.secret");
 	}
 
 	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
